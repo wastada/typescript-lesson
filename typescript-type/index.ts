0e0db5edce1357a0fb96fdf673bd4de518e0137b
shingo <shingo.iwata4050927@gmail.com>
2022-06-05 17:22:06 +0900

    new push

---
+++ typescript-type/index.ts
@@ -0,0 +1,8 @@
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single:  string ='hello';
+let double:  string ="hello";
+let back: string = 'hello';
+
